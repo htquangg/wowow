bcca43756e87e2907cb0231559b515c206c9a237
ALEX Huynh <htquangg@gmail.com>
2023-11-10 13:24:44 +0700

    feat: docker compose http server

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ docker-compose --env-file .env.docker -f ./deployments/docker-compose.dev.yml do
 
 docker-compose --env-file .env.docker -f ./deployments/docker-compose.dev.yml up -d
 
+npm run -g live-server
+
 yarn
 
-docker restart wow-rpc-server
+docker restart wow-rpc-server wow-http-server
